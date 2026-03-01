8efbd5b76ffd104583a90c23afa2e303fd867c82
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-01 14:26:05 +0200

    security: successfully registered Android Play Integrity and initialized App Check in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,19 @@ import 'package:chat_app_with_ai/Router/app_router.dart';
 import 'package:chat_app_with_ai/Router/app_routes.dart';
 import 'package:chat_app_with_ai/firebase_options.dart';
 import 'package:chat_app_with_ai/utilities/theme/app_theme.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
+    appleProvider: AppleProvider.debug,
+    // providerAndroid: AndroidAppCheckProvider.playIntegrity,
+  );
 
   runApp(const MyApp());
 }
